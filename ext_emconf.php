fd51427a729698acfd082878478ddc3985ce1f2d
jonathanheilmann <mail@jonathan-heilmann.de>
2015-10-19 20:48:59 +0200

    [RELEASE] Release of version 0.4.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,39 +1,39 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext "jh_simple_youtube".
-#
-# Auto generated 12-08-2015 22:47
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
+/***************************************************************
+ * Extension Manager/Repository config file for ext "jh_simple_youtube".
+ *
+ * Auto generated 19-10-2015 20:27
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
 
-$EM_CONF[$_EXTKEY] = array(
+$EM_CONF[$_EXTKEY] = array (
 	'title' => 'Simple Youtube',
 	'description' => 'Simply add a youtube-video as a responsive content-element. The embedded video is compatible to mobile devices (html5).',
 	'category' => 'plugin',
-	'shy' => 1,
-	'version' => '0.4.0',
-	'dependencies' => '',
-	'conflicts' => '',
-	'priority' => '',
-	'loadOrder' => '',
-	'TYPO3_version' => '6.0.0-6.2.99',
-	'PHP_version' => '',
-	'module' => '',
+	'version' => '0.4.1',
 	'state' => 'stable',
-	'uploadfolder' => 1,
+	'uploadfolder' => true,
 	'createDirs' => '',
-	'modify_tables' => '',
-	'clearcacheonload' => 1,
-	'lockType' => '',
+	'clearcacheonload' => true,
 	'author' => 'Jonathan Heilmann',
 	'author_email' => 'mail@jonathan-heilmann.de',
 	'author_company' => '',
-	'CGLcompliance' => '',
-	'CGLcompliance_note' => '',
+	'constraints' => 
+	array (
+		'depends' => 
+		array (
+			'typo3' => '6.2.99-7.6.99',
+		),
+		'conflicts' => 
+		array (
+		),
+		'suggests' => 
+		array (
+		),
+	),
 );
 
-?>
