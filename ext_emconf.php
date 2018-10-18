644199f889e4b125db5764ff59d0158f09cf664d
Jonathan Heilmann <mail@jonathan-heilmann.de>
2018-10-18 20:04:12 +0200

    [TASK] Set version to 0.7.0-dev

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "jh_simple_youtube".
  *
- * Auto generated 19-10-2015 20:27
+ * Auto generated 18-10-2018 19:50
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -11,29 +11,29 @@
  ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = array (
-	'title' => 'Simple Youtube',
-	'description' => 'Simply add a youtube-video as a responsive content-element. The embedded video is compatible to mobile devices (html5).',
-	'category' => 'plugin',
-	'version' => '0.6.0',
-	'state' => 'stable',
-	'uploadfolder' => true,
-	'createDirs' => '',
-	'clearcacheonload' => true,
-	'author' => 'Jonathan Heilmann',
-	'author_email' => 'mail@jonathan-heilmann.de',
-	'author_company' => '',
-	'constraints' =>
-	array (
-		'depends' =>
-		array (
-			'typo3' => '7.6.0-8.7.99',
-		),
-		'conflicts' =>
-		array (
-		),
-		'suggests' =>
-		array (
-		),
-	),
+  'title' => 'Simple Youtube',
+  'description' => 'Simply add a youtube-video as a responsive content-element. The embedded video is compatible to mobile devices (html5).',
+  'category' => 'plugin',
+  'version' => '0.7.0-dev',
+  'state' => 'stable',
+  'uploadfolder' => true,
+  'createDirs' => '',
+  'clearcacheonload' => true,
+  'author' => 'Jonathan Heilmann',
+  'author_email' => 'mail@jonathan-heilmann.de',
+  'author_company' => '',
+  'constraints' => 
+  array (
+    'depends' => 
+    array (
+      'typo3' => '7.6.0-8.7.99',
+    ),
+    'conflicts' => 
+    array (
+    ),
+    'suggests' => 
+    array (
+    ),
+  ),
 );
 
